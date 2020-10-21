e42920021b7baa4943b14d90543c97bf40416dde
schakravarti <schakravarti930@gmail.com>
2020-10-21 22:33:37 +0530

    [Swapnil] Add : Find maximum among three strings

--- MaxUsingGenericsTest/UnitTest1.cs
+++ MaxUsingGenericsTest/UnitTest1.cs
@@ -5,33 +5,34 @@ namespace MaxUsingGenericsTest
     [TestClass]
     public class UnitTest1
     {
+        //TC1.1
         [TestMethod]
         public void Maximum_Value_PassedAt_First_Parameter_Should_Return_Maximum_Value()
         {
-            float firstValue = 30.5f, secondValue = 20.5f, thirdValue = 10.5f;
-            float expected = 30.5f;
+            string firstValue = "Carrot", secondValue = "Banana", thirdValue = "Apple";
+            string expected = "Carrot";
             FindMaximum find = new FindMaximum();
-            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expected, actual);
         }
         //TC1.2
         [TestMethod]
         public void Maximum_Value_PassedAt_Second_Parameter_Should_Return_Maximum_Value()
         {
-            float firstValue = 20.5f, secondValue = 30.5f, thirdValue = 10.5f;
-            float expected = 30.5f;
+            string firstValue = "Banana", secondValue = "Carrot", thirdValue = "Apple";
+            string expected = "Carrot";
             FindMaximum find = new FindMaximum();
-            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expected, actual);
         }
         //TC1.3
         [TestMethod]
         public void Maximum_Value_PassedAt_Third_Parameter_Should_Return_Maximum_Value()
         {
-            float firstValue = 20.5f, secondValue = 10.5f, thirdValue = 30.5f;
-            float expected = 30.5f;
+            string firstValue = "Apple", secondValue = "Banana", thirdValue = "Carrot";
+            string expected = "Carrot";
             FindMaximum find = new FindMaximum();
-            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
+            string actual = find.MaximumStringNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expected, actual);
         }
     }
